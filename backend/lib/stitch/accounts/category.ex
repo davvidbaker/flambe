1af13e4dc9afacd9a8e966c47e7363f34dcdfc70
David Baker <David@MacBook-Pro-5.local>
2017-10-28 04:26:20 -0400

    activities_categories table

--- backend/lib/stitch/accounts/category.ex
+++ backend/lib/stitch/accounts/category.ex
@@ -2,12 +2,14 @@ defmodule Stitch.Accounts.Category do
   use Ecto.Schema
   import Ecto.Changeset
   alias Stitch.Accounts.{Category, User}
+  alias Stitch.Traces.{Activity}
 
 
   schema "categories" do
     field :color, :string
     field :name, :string
     belongs_to :user, User
+    many_to_many :activities, Activity, join_through: "activities_categories"
 
     timestamps()
   end
