72e903f4bf0def97df0c38b1270db6e4269dc7ec
David Baker <David@MacBook-Pro-5.local>
2017-10-28 01:51:07 -0400

    return list of traces with user

--- backend/lib/stitch_web/views/user_view.ex
+++ backend/lib/stitch_web/views/user_view.ex
@@ -13,9 +13,15 @@ defmodule StitchWeb.UserView do
     def render("user.json", %{user: user}) do
       # ⚠️ this might not be how you're supposed to do this, email is really not part of the user, but the user credentials. This could be refactored.
       user_with_email = Stitch.Repo.preload(user, :credential)
+
+      # ⚠️ Is this where this should happen? I doubt it.
+      traces = Stitch.Traces.list_user_traces(user.id);
+      
       %{id: user_with_email.id,
         name: user_with_email.name,
-        email: user_with_email.credential.email}
+        email: user_with_email.credential.email,
+        traces: traces
+      }
     end
 
 end
