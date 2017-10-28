20a312f61b9373c0b15713afbeaca81863579740
David Baker <davvidbaker@gmail.com>
2017-10-28 00:41:46 -0400

    all tests pass

---
+++ backend/priv/repo/migrations/20171028015124_event_activity_can_be_null.exs
@@ -0,0 +1,11 @@
+defmodule Stitch.Repo.Migrations.DeleteEventsWhenDeletingActivity do
+  use Ecto.Migration
+
+  def change do
+    # ⚠️ hacky
+    drop constraint(:events, "events_activity_id_fkey")
+    alter table(:events) do
+      modify :activity_id, references(:activities, on_delete: :delete_all), null: true
+    end
+  end
+end
