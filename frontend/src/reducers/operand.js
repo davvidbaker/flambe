6e162c7cbb0da114a77bd17935d9144126808b11
David Baker <davvidbaker@gmail.com>
2017-11-21 12:00:00 -0500

    fix operand status

--- frontend/src/reducers/operand.js
+++ frontend/src/reducers/operand.js
@@ -1,4 +1,9 @@
-import { BLOCK_FOCUS } from 'actions';
+import {
+  BLOCK_FOCUS,
+  ACTIVITY_END,
+  ACTIVITY_SUSPEND,
+  ACTIVITY_RESUME,
+} from 'actions';
 
 function operand(state = null, action) {
   switch (action.type) {
@@ -8,6 +13,24 @@ function operand(state = null, action) {
         activity_id: action.activity_id,
         activityStatus: action.activityStatus,
         thread_id: action.thread_id,
+      };
+
+    case ACTIVITY_END:
+      return {
+        ...state,
+        activityStatus: 'complete',
+      };
+
+    case ACTIVITY_SUSPEND:
+      return {
+        ...state,
+        activityStatus: 'suspended',
+      };
+
+    case ACTIVITY_RESUME:
+      return {
+        ...state,
+        activityStatus: 'active',
       };
 
     default:
