44ff3eff609204caa093d64be83f26db3cb66067
davvidbaker <davvidbaker@gmail.com>
2018-05-06 14:20:06 -0500

    logo/login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Button from './Button';
+import Logo from './Logo';
 
+const CenterFlex = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  outline: 5px solid #FFD12F;
+  outline-offset:-10px;
+  border: 5px solid #ff5826;
+  
+  box-sizing: outline-box;
+
+  .inner {
+    padding: 20px;
+    text-align: center;
+    /* border: 3px solid #FFD12F; */
+    z-index: -1;
+  }
+`;
+
+const Padded = styled.div`
+  /* 🤔  maybe bad/weird pattern here*/
+  padding: 30px;
+  background: 
+`;
 const Login = () => (
   // fetch(`${SERVER}/auth/github`);
-  <div>
-    <h1>Login please</h1>
-    <a
-      onClick={() =>
-        window.open(
-          `${SERVER}/auth/github`,
-          'foo',
-          'width=200, height=300, top=0'
-        )
-      }
-      href={'#'}
-    >
-      Log in with Github
-    </a>
-  </div>
+  <CenterFlex>
+    <div className="inner">
+      <Padded>
+        <Logo size={90} className="padded" />
+      </Padded>
+      <h1>Login please</h1>
+      <a
+        onClick={() =>
+          window.open(
+            `${SERVER}/auth/github`,
+            'foo',
+            'width=200, height=300, top=0'
+          )
+        }
+        href={'#'}
+      >
+        Log in with Github
+      </a>
+    </div>
+  </CenterFlex>
 );
 export default Login;
