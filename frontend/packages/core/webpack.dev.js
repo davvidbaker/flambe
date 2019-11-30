e36a7a7c66c5c2cb304768d0084b699fb3274c2c
davvidbaker <davvidbaker@gmail.com>
2019-11-29 20:06:00 -0600

    idk

--- frontend/packages/core/webpack.dev.js
+++ frontend/packages/core/webpack.dev.js
@@ -1,7 +1,7 @@
+const path = require('path');
+
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
@@ -9,11 +9,11 @@ module.exports = merge(common, {
   mode: 'development',
   // potentially insecure way to view on remote computer
   devServer: {
-    host: "localhost",
+    host: 'localhost',
     port: 8081,
     hot: true,
     historyApiFallback: true,
-    https: true
+    https: true,
   },
 
   devtool: 'inline-cheap-source-map',
@@ -21,23 +21,23 @@ module.exports = merge(common, {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js', // use npm run build to build production bundle
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
 
   plugins: [
     new webpack.DefinePlugin({
       SERVER: "'http://localhost:4000'",
       SOCKET_SERVER: "'ws://localhost:4000'",
-      NODE_ENV: "'development'"
+      NODE_ENV: "'development'",
     }),
 
     // This plugin will cause the relative path of the module to be displayed when HMR is enabled.
     new webpack.NamedModulesPlugin(),
 
-    new webpack.HotModuleReplacementPlugin({})
+    new webpack.HotModuleReplacementPlugin({}),
     // new HtmlWebpackPlugin({
     //   template: './index.dev.ejs',
     //   title: 'Flame-Chart'
     // })
-  ]
+  ],
 });
