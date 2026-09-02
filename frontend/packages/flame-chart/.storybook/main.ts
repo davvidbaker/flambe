8814daa786a668d6381363498ccdaa8b86dcd548
David Baker <drbaker2@gmail.com>
2026-09-01 23:32:20 -0600

    Update Storybook 9 configuration

--- frontend/packages/flame-chart/.storybook/main.ts
+++ frontend/packages/flame-chart/.storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
