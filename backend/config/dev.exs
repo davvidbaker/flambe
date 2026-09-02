c6b04f632ab6b2c1558b0ad844f9acef8d82b71c
david.baker <davvidbaker@gmail.com>
2026-09-02 14:17:33 -0600

    Allow LAN access in development

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -20,7 +20,8 @@ config :flambe_next, FlambeNextWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   # The legacy Phoenix app remains on 4000 during the migration.
-  http: [ip: {127, 0, 0, 1}, port: 4001],
+  # http: [ip: {127, 0, 0, 1}, port: 4001],
+  http: [ip: {0, 0, 0, 0}, port: 4001],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
