726ae446f673d260fd672e413c077eeaed69de16
david.baker <davvidbaker@gmail.com>
2026-09-16 12:00:00 -0600

    Fix flame placement on title

--- frontend/packages/core/src/components/Logo/src/index.tsx
+++ frontend/packages/core/src/components/Logo/src/index.tsx
@@ -18,7 +18,7 @@ const H1 = styled.h1<{ $isAnimated?: boolean; $size: number }>`
     position: absolute;
     right: 0;
     transform-origin: top right;
-    transform: translate(23%, 23%) scale(0.5) rotate(30deg);
+    transform: translate(27%, 23%) scale(0.5) rotate(30deg);
     z-index: -2;
   ${props =>
     (!props.$isAnimated
